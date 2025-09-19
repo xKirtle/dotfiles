4a755356b5a45e3594ff2e1a80ee286e4b7b7e5f
Rodrigo Martins <rodm.martins@proton.me>
2025-09-19 11:09:37 +0100

    Fix git-work fish function

--- .config/fish/functions/git-work.fish
+++ .config/fish/functions/git-work.fish
@@ -1,6 +1,6 @@
 function git-work --description "Configure current Git repo for work commits"
 # Make sure we’re inside a Git repository
-    if not git rev-parse --is-inside-work-tree ^/dev/null
+    if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
         echo "Not a git repository."
         return 1
     end
@@ -10,7 +10,7 @@ function git-work --description "Configure current Git repo for work commits"
     git config user.signingkey <WORK_GPG_KEY_ID>
     git config commit.gpgSign true
 
-    echo "Configured this repo for WORK identity:"
+    echo "Configured this repository to use the following identity:"
     git config --get user.name
     git config --get user.email
     git config --get user.signingkey
