e363e877f66b33f485f8db457af54221fc020b79
Rodrigo Martins <rodm.martins@proton.me>
2025-09-19 11:09:18 +0100

    Fix git-personal fish function

--- .config/fish/functions/git-personal.fish
+++ .config/fish/functions/git-personal.fish
@@ -1,6 +1,6 @@
 function git-personal --description "Configure current Git repo for personal commits"
     # Make sure we’re inside a Git repository
-    if not git rev-parse --is-inside-work-tree ^/dev/null
+    if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
         echo "Not a git repository."
         return 1
     end
@@ -10,7 +10,7 @@ function git-personal --description "Configure current Git repo for personal com
     git config user.signingkey <PERSONAL_GPG_KEY_ID>
     git config commit.gpgSign true
 
-    echo "Configured this repo for PERSONAL identity:"
+    echo "Configured this repository to use the following identity:"
     git config --get user.name
     git config --get user.email
     git config --get user.signingkey
